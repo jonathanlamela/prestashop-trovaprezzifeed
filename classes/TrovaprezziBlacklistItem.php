2222a252a69c8c9b4ce713118ef0ae5da363d0c1
Jonathan La Mela <jonathan.la.mela@gmail.com>
2025-09-03 18:55:16 +0200

    bug fix

--- classes/TrovaprezziBlacklistItem.php
+++ classes/TrovaprezziBlacklistItem.php
@@ -7,7 +7,7 @@ class TrovaprezziBlacklistItem extends ObjectModel
 
     public static $definition = array(
         'table' => 'trovaprezzifeed_blacklist',
-        'primary' => 'id_trovaprezzi_blacklist',
+        'primary' => 'id_trovaprezzifeed_blacklist',
         'fields' => array(
             'internal_code' => array('type' => self::TYPE_STRING, 'validate' => 'isString', 'required' => true, 'size' => 100),
         ),
